c5d20d9b5d96d0c4ef0cd7e25d616f354cbae54e
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-06-03 09:55:13 +0300

    Create ConfigPublishCommand.php

---
+++ src/Commands/ConfigPublishCommand.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace M2Collective\CrawlerDetectManager\Commands;
+
+use Illuminate\Console\Command;
+
+final class ConfigPublishCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'crawler-detect-manager:publish-config';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Publishing the configuration file';
+
+    /**
+     * @return void
+     */
+    public function handle() : void
+    {
+        $this->call('vendor:publish', [
+            '--tag' => 'crawler-detect-manager-publish-config'
+        ]);
+    }
+}
