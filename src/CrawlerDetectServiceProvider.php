055e20f72f10d7d15829c15adab98b2d05bbb3f1
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-06-07 14:24:22 +0300

    Update CrawlerDetectServiceProvider.php

--- src/CrawlerDetectServiceProvider.php
+++ src/CrawlerDetectServiceProvider.php
@@ -6,11 +6,11 @@ namespace M2Collective\CrawlerDetect;
 use M2Collective\CrawlerDetect\Console\Commands\ConfigPublishCommand;
 use M2Collective\CrawlerDetect\View\Directives\IsCrawlerDirective;
 use M2Collective\PackageKit\Support\AbstractServiceProvider;
-use M2Collective\PackageKit\Support\Providers\Traits\RegisterDirectivesTrait;
+use M2Collective\PackageKit\Support\Providers\Concerns\RegisterDirectives;
 
 final class CrawlerDetectServiceProvider extends AbstractServiceProvider
 {
-    use RegisterDirectivesTrait;
+    use RegisterDirectives;
 
     /**
      * @return void
